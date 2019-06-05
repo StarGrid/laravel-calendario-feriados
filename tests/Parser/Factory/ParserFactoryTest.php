f6c822d8bf9265ffbe75a1fdeeb77e3785abe50e
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2019-06-05 15:39:11 -0300

    feature: Implement parser factory tests

---
+++ tests/Parser/Factory/ParserFactoryTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace StarGrid\LaravelHolidayCalendar\Parser\Factory;
+
+use PHPUnit\Framework\TestCase;
+
+use StarGrid\LaravelHolidayCalendar\Api\ResponseTypeEnum;
+use StarGrid\LaravelHolidayCalendar\Parser\JsonParser;
+use StarGrid\LaravelHolidayCalendar\Parser\XmlParser;
+
+/**
+ * Class ParserFactoryTest
+ * @package StarGrid\LaravelHolidayCalendar\Parser\Factory
+ *
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
+class ParserFactoryTest extends TestCase
+{
+    /**
+     * Testa sucesso ao contruir uma parser to tipo Json.
+     */
+    public function testMakeJsonParser()
+    {
+        $parserFactory = new ParserFactory;
+
+        $jsonParser = $parserFactory->makeParser(ResponseTypeEnum::JSON_RESPONSE());
+
+        $this->assertEquals(new JsonParser, $jsonParser);
+    }
+
+    /**
+     * Testa sucesso ao contruir uma parser to tipo XML.
+     */
+    public function testMakeXmlParser()
+    {
+        $parserFactory = new ParserFactory;
+
+        $xmlParser = $parserFactory->makeParser(ResponseTypeEnum::XML_RESPONSE());
+
+        $this->assertEquals(new XmlParser(), $xmlParser);
+    }
+}
