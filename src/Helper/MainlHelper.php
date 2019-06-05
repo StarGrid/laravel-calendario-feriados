9bf526cf8de3a6494988a2c4c5292d2b65477134
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2019-06-05 18:10:27 -0300

    feature: Implements main helper

---
+++ src/Helper/MainlHelper.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace StarGrid\LaravelHolidayCalendar\Helper;
+
+/**
+ * Class MainlHelper
+ * @package StarGrid\LaravelHolidayCalendar\Helper
+ *
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
+class MainlHelper
+{
+    /**
+     * Normaliza o nome de cidades de acordo com o esperado pela API de feriados.
+     *
+     * @param string $cityName Nome da cidade a ser normalizado.
+     * @return string Nome da cidade formatado com os padrões esperados pela API.
+     */
+    public static function normalizeCityName(string $cityName): string
+    {
+        $table = [
+            'Š' => 'S', 'š' => 's', 'Ð' => 'Dj', 'd' => 'dj', 'Ž' => 'Z', 'ž' => 'z', 'C' => 'C', 'c' => 'c', 'C' => 'C', 'c' => 'c',
+            'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C', 'È' => 'E', 'É' => 'E',
+            'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O',
+            'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Þ' => 'B', 'ß' => 'Ss',
+            'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'a', 'ç' => 'c', 'è' => 'e', 'é' => 'e',
+            'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'o', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o',
+            'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ý' => 'y', 'ý' => 'y', 'þ' => 'b',
+            'ÿ' => 'y', 'R' => 'R', 'r' => 'r',
+        ];
+
+        $cityName = strtr($cityName, $table);
+
+        $formattedCityNames = [];
+        foreach (explode(' ', $cityName) as $name) {
+            $formattedCityNames[] = ucfirst($name);
+        }
+
+        $cityName = implode('_', $formattedCityNames);
+
+        return $cityName;
+    }
+}
