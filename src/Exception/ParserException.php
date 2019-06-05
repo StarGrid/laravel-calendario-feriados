59ff4975cb7dec1c946dcd154a7028aa75e9af23
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2019-06-05 14:36:16 -0300

    feature: Create parser exception class

---
+++ src/Exception/ParserException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace StarGrid\LaravelHolidayCalendar;
+
+/**
+ * Class ParserException
+ * @package StarGrid\LaravelHolidayCalendar
+ *
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
+class ParserException extends \Exception
+{
+    
+}
