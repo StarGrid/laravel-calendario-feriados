a512eb052650ececd5c0af6d69405d7a489c85ab
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2019-06-07 15:50:27 -0300

    feature: Update config file

--- config/holiday_calendar.php
+++ config/holiday_calendar.php
@@ -4,7 +4,7 @@
  * Configurações padrão da intregração com http://www.calendario.com.br/api_feriados_municipais_estaduais_nacionais.php
  */
 return [
-    'token' => env('LARAVEL_HOLIDAY_CALENDAR_TOKEN', ''),
+    'token' => env('LARAVEL_HOLIDAY_CALENDAR_TOKEN'),
 
     'url_api' => env('LARAVEL_HOLIDAY_CALENDAR_URL_API', 'https://api.calendario.com.br')
 ];
